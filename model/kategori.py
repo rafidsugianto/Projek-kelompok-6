69ad2d035051de634485c4e3b8b72613202a67dd
rafidsugianto <rafidsugianto17@gmail.com>
2025-11-29 15:59:10 +0700

    Update kategori.py

--- model/kategori.py
+++ model/kategori.py
@@ -1,4 +1,15 @@
+from model import database as db
+
 class kategori:
     def __init__(self, nama_kategori, kode_kategori):
         self.nama_kategori = nama_kategori
         self.kode_kategori = kode_kategori
+
+    def simpan(self):
+        query = "INSERT INTO kategori (nama_kategori, kode_kategori) VALUES (%s, %s)"
+        values = (self.nama_kategori, self.kode_kategori)
+        db.insert_data(query, values)
+        print("Kategori berhasil disimpan!")
+
+k = kategori("Elektronik", "EL001")
+k.simpan()
