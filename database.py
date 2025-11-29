75dd8dd77e936291244b9b4360234b6cbb992a23
rafidsugianto <rafidsugianto17@gmail.com>
2025-11-29 15:56:54 +0700

    commit

--- database.py
+++ database.py
@@ -4,9 +4,17 @@ def connect():
     return mysql.connector.connect(
         host="localhost",
         user="root",
-        password="",          
-        database="database_toko"  
+        password="",   
+        database="database_toko"
     )
+
+def insert_data(query, values):
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute(query, values)
+    db.commit()
+    cursor.close()
+    db.close()
 
 def get_data(query, values=None):
     db = connect()
@@ -16,6 +24,3 @@ def get_data(query, values=None):
     cursor.close()
     db.close()
     return result
-
-# print(db.mydb)
-        
